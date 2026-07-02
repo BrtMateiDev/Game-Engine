e85db249985f54fac574aea07817bb798bcaf8d3
BrtMateiDev <brtmateidev007@gmail.com>
2026-07-02 18:09:28 +0300

    Window initialization

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,27 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
-using namespace std;
+
 int main() {
-    cout << "Hello world";
+#if defined(__linux__)
+    glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+#endif
+
+    if (!glfwInit()) return -1;
+
+    //The window acts as an object stored in a pointer
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "GameDevelopmentProject", nullptr, nullptr);
+
+    if (window == nullptr) {
+        std::cout<<"Error creating window"<<'\n';
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents(); //Events like the close button
+    }
+
+    glfwTerminate();
+
     return 0;
 }
